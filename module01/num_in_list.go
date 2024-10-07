a02fa6fbc89b6d0c1ca19f8bbad09e13dad52ac4
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2024-10-07 20:32:10 +0100

    Add solution with for _,i where i becomes the value instead of index

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -4,6 +4,16 @@ package module01
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
 
+	for _, i := range list {
+		if i == num {
+			return true
+		}
+	}
+	return false
+}
+
+func NumInList2(list []int, num int) bool {
+
 	count := len(list)
 	for i := 0; i < count; i++ {
 		if list[i] == num {
