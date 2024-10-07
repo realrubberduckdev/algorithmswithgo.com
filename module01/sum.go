e5ce1132a2950b8dfe4e9194446bf0dce2c6f9ba
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2024-10-07 20:56:56 +0100

    Add sum solution

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,9 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	var sum int = 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
