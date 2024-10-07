2e73228b8e8d7a35988ad34d331e484fa224be52
Dushyant Priyadarshee <dushyantpriyadarshee@gmail.com>
2024-10-07 21:06:03 +0100

    Use recursion

--- module01/sum.go
+++ module01/sum.go
@@ -3,9 +3,8 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	var sum int = 0
-	for _, num := range numbers {
-		sum += num
+	if len(numbers) == 0 {
+		return 0
 	}
-	return sum
+	return numbers[0] + Sum(numbers[1:])
 }
